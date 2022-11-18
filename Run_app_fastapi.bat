6a5b50b08c6c369f7ea5e82c85043e7f6ad532d9
enterus01 <buinguyenthanhdanh@gmail.com>
2022-11-19 01:11:43 +0700

    update logs
    Logs into logs folder

--- Run_app_fastapi.bat
+++ Run_app_fastapi.bat
@@ -1,2 +1,4 @@
-cd C:\Python\FastAPI
-C:\Users\Administrator\AppData\Local\Programs\Python\Python310\Scripts\uvicorn.exe app_api:app --host 0.0.0.0 --port 8080
+cd C:\Python\FastAPI\logs
+del logfile.log
+cd C:\Python\FastAPI\
+C:\Users\Administrator\AppData\Local\Programs\Python\Python310\Scripts\uvicorn.exe app_api:app --host 0.0.0.0 --port 8080 --workers 4 --log-config config_log.ini --use-colors
